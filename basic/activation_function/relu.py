f7c779dba1c78c19ef8a22c443f83234e280b494
JiaweiMao <jiaweimao2019@outlook.com>
2022-10-25 15:49:32 +0800

    add 2_训练

--- basic/activation_function/relu.py
+++ basic/activation_function/relu.py
@@ -12,13 +12,19 @@ def relu_derivative(x):
     return np.greater(x, 0).astype(int)
 
 
+def sigmoid(x):
+    pass
+
+
 xs = np.arange(-5, 6)
 
-fig, (ax1, ax2) = plt.subplots(2, 1)  # type:figure.Figure, (axes.Axes, axes.Axes)
+fig, (ax1, ax2) = plt.subplots(2,
+                               1)  # type:figure.Figure, (axes.Axes, axes.Axes)
 
 
 def set_splines(ax):
-    ax.spines[["left", "bottom"]].set_position(("data", 0))  # 通过值来设置 x 和 y 轴位置，都设置 0 位置
+    ax.spines[["left", "bottom"]].set_position(
+        ("data", 0))  # 通过值来设置 x 和 y 轴位置，都设置 0 位置
     ax.spines[["top", "right"]].set_visible(False)  # 隐藏
 
 
