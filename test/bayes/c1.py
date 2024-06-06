adeb6694b39f390331246e1c45e7ba6a5328a420
jiawei mao <jiaweim_philo@hotmail.com>
2024-06-06 19:40:27 +0800

    update

--- test/bayes/c1.py
+++ test/bayes/c1.py
@@ -1,5 +1,18 @@
-def P(S, A):
-    if set(A).issubset(set(S)):
-        return len(A) / len(S)
-    else:
-        return 0
+import numpy as np
+
+def markov():
+    init_array = np.array([0.1,0.2,0.7])
+    transfer_matrx = np.array([
+        [0.9,0.075,0.025],
+        [0.15, 0.8, 0.05],
+        [0.25,0.25,0.5]
+    ])
+
+    restmp = init_array
+    for i in range(25):
+        res = np.dot(restmp, transfer_matrx) # 矩阵乘法
+        print(i, "\t", res)
+        restmp = res
+
+if __name__ == "__main__":
+    markov()
